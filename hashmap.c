eafb0fa27e7e54325f9b79648d676a3b7c049cd6
Your Name <you@example.com>
2022-04-10 21:01:56 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -118,7 +118,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-    for (int i = 0 ; i < map->capacity - 1 ; i = ((i + 1) % map->capacity)){
+    for (int i = 0 ; i < map->capacity - 1 ; i++){
         if(map -> buckets[i]!= NULL && map -> buckets[i] -> key != NULL){
             map -> current = i;
             return map -> buckets[i];
@@ -128,7 +128,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-    for (long i = map->current + 1 ; i < map->capacity - 1 ; i = ((i + 1) % map->capacity)){
+    for (long i = map->current + 1 ; i < map->capacity - 1 ; i++){
         if(map -> buckets[i]!= NULL && map -> buckets[i] -> key != NULL){
             map -> current = i;
             return map -> buckets[i];
