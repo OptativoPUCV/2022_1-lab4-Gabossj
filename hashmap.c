ecdfcc7b1d45c7d3942f0e217ffef96a240bb3c6
Your Name <you@example.com>
2022-04-10 23:29:19 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -73,7 +73,6 @@ void enlarge(HashMap * map) {
     // for (int i=0; i < capacityMax; i++){
     //     if (oldBuckets[i]){
     //         insertMap(map, oldBuckets[i]-> key, oldBuckets[i] -> value);
-    //         break;
     //     }
     // }
     
@@ -131,24 +130,24 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 Pair * firstMap(HashMap * map) {
     
-//     for(long i = 0 ; i < map -> size; i++){
-//         if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
-//             map -> current = i;
-//             return map -> buckets[i];
-//         }
-//         return map -> buckets[i];
-//     }
+    for(long i = 0 ; i < map -> size; i++){
+        if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL){
+            map -> current = i;
+            return map -> buckets[i];
+        }
+        return map -> buckets[i];
+    }
     return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
     
-//     for(long i = map -> current ; i < map -> capacity ; i++){
-//         if(map->buckets[i]!=NULL && map->buckets[i]->key !=NULL){
-//             map -> current = (i + 1);
-//             return map -> buckets[i];
-//         }
-//     }
+    for(long i = map -> current ; i < map -> capacity ; i++){
+        if(map->buckets[i]!=NULL && map->buckets[i]->key !=NULL){
+            map -> current = (i + 1);
+            return map -> buckets[i];
+        }
+    }
     
     return NULL;
 }
